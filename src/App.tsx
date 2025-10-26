c0179670886d65800a0becf0d7cd7585b9cc28a1
Junya <junjia.liu@pm.me>
2025-10-26 03:23:51 +0000

    Add footer (i hate you css)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {OrpheusFlag} from './components/OrpheusFlag';
 import {Footer} from './components/Footer';
+import {NavBar} from "./components/NavBar.tsx";
 
 function App() {
 	return (
 		<div>
 			<div>
 				<OrpheusFlag />
+			</div>
+			<div className="fixed top-0 max-w-48 right-4 sm:right-5 md:right-10">
+				<NavBar />
 			</div>
 			<div className="flex justify-center items-center flex-grow">
 				<div className="mt-16 text-slate-950 mx-8 md:max-w-6xl font-mono">
