14b02c33d1d57de4839aa80a1c2cf2fca1715e76
Junya <junjia.liu@pm.me>
2025-11-04 00:36:25 +0000

    Add some Qs for FAQ

--- src/App.tsx
+++ src/App.tsx
@@ -117,16 +117,36 @@ function App() {
 					</h2>
 					<div className="w-[90%] min-h-[80vh] mx-auto bg-white/10 backdrop-blur-sm shadow-sm border-inset border-4 border-black p-8">
 						<QuestionAnswer
-							question="ABC"
-							answer="Foo Bar"
+							question="How much time do I need to spend on my game?"
+							answer="At least 5 hours! <b>Hackatime is required.</b><br>Your game <b>must be shippable</b> and in a playable state, with the code open-sourced and the game's binary file."
 						/>
 						<QuestionAnswer
-							question="ABC"
-							answer="Foo Bar"
+							question="What are the prizes :O?!"
+							answer="You get <b>$5 per hour tracked</b> that goes towards your prize, ranging from LEGO® sets, LEGO® pieces to brick-related merchandise, puzzles and Blahajs!<br>Spend <u>200 hours</u> and you could get a <b>Millenium Falcon™ 75192 Ultimate Collector series</b>!"
 						/>
 						<QuestionAnswer
-							question="ABC"
-							answer="Foo Bar"
+							question="When does this end?"
+							answer="TBA."
+						/>
+						<QuestionAnswer
+							question="Can I double dip with [insert X here]?"
+							answer="No."
+						/>
+						<QuestionAnswer
+							question="Do I need a Roblox account for this?"
+							answer="Yes, but you can <u><a href='https://www.roblox.com'>signup</a></u> for one. <u><a href='https://youtu.be/97dyt7MXWpo?t=26'>It's FREE!</a></u>"
+						/>
+						<QuestionAnswer
+							question="Can I ship a project I previously worked on / an old game?"
+							answer="Unfortunately it needs to be made for this YSWS :("
+						/>
+						<QuestionAnswer
+							question="Can I use plugins, models, assets found on the marketplace?"
+							answer="Sure! As long as you're doing a majority of the work (e.g., not stealing code from free models)."
+						/>
+						<QuestionAnswer
+							question="I have a latta questions to be answered!!"
+							answer="Join the <b><u><a href='https://hackclub.slack.com/archives/C091BU30NGK'>#bricklayer</a></u></b> channel on the Hack Club Slack. We don't bite, promise!!"
 						/>
 					</div>
 				</div>
--- src/components/QuestionAnswer.tsx
+++ src/components/QuestionAnswer.tsx
@@ -5,12 +5,14 @@ interface Properties {
 export function QuestionAnswer({ question, answer }: Properties) {
 	return (
 		<div className="mb-8 last:mb-0">
-			<h3 className="text-xl sm:text-2xl md:text-3xl font-bold text-black mb-3">
-				{question}
-			</h3>
-			<p className="text-base sm:text-lg md:text-xl text-black leading-relaxed">
-				{answer}
-			</p>
+			<h3
+				className="text-xl sm:text-2xl md:text-3xl font-bold text-black mb-3"
+				dangerouslySetInnerHTML={{ __html: question }}
+			/>
+			<p
+				className="text-base sm:text-lg md:text-xl text-black leading-relaxed"
+				dangerouslySetInnerHTML={{ __html: answer }}
+			/>
 		</div>
 	);
 }
