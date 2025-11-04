cf48497d275b2a30d3e191041d1fa62f166efb80
Junya <junjia.liu@pm.me>
2025-11-04 22:04:53 +0000

    Edit prose classes

--- src/layouts/DocPage.tsx
+++ src/layouts/DocPage.tsx
@@ -16,7 +16,7 @@ export function DocPage({ Content, SideBar }: Properties) {
 				<aside className="hidden sm:block w-48 fixed h-full">
 					<SideBar />
 				</aside>
-				<main className="backdrop-blur-xs text-black flex-1 p-8 prose-ul:list-disc prose-ol:list-decimal prose-sm prose-headings:my-2 prose-headings:font-semibold mx-auto sm:ml-48">
+				<main className="backdrop-blur-xs text-black flex-1 p-8 prose-ul:list-disc prose-ol:list-decimal prose-base prose-p:font-medium prose-headings:my-1 prose-headings:font-semibold mx-auto sm:ml-48">
 					<Content />
 				</main>
 			</div>
