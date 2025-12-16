eb5ee1b94f0daca1694b72d1048f44542f039e68
Junya <junjia.liu@pm.me>
2025-12-16 00:51:20 +0000

    Fix full-width clickable areas

--- src/components/BrickButton.tsx
+++ src/components/BrickButton.tsx
@@ -8,13 +8,15 @@ interface Properties {
 
 export function BrickButton({ href, text, className = ''}: Properties) {
 	return (
-		<Link to={href} className="flex justify-center">
-			<p
-				className={`text-xl sm:text-2xl text-center bg-red-500 text-slate-50 mt-4 mb-8 mx-w-54 px-3 py-4 rounded-sm font-semibold border-black border-4 bg-repeat bg-center ${className}`}
-				style={{ backgroundImage: 'url(/box_stud_bright_red.png)' }}
-			>
-				{text}
-			</p>
-		</Link>
+		<div className="flex justify-center">
+			<Link to={href}>
+				<p
+					className={`text-xl sm:text-2xl text-center bg-red-500 text-slate-50 mt-4 mb-8 mx-w-54 px-3 py-4 rounded-sm font-semibold border-black border-4 bg-repeat bg-center ${className}`}
+					style={{ backgroundImage: 'url(/box_stud_bright_red.png)' }}
+				>
+					{text}
+				</p>
+			</Link>
+		</div>
 	);
 }
