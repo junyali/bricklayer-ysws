0cf0e240c722fc64a44dd1b9c81582317e1121dc
Junya <junjia.liu@pm.me>
2025-12-16 01:06:27 +0000

    Fix hover underline for a tags

--- src/layouts/DocPage.tsx
+++ src/layouts/DocPage.tsx
@@ -16,7 +16,7 @@ export function DocPage({ Content, SideBar }: Properties) {
 				<aside className="hidden sm:block w-48 fixed h-full">
 					<SideBar />
 				</aside>
-				<main className="backdrop-blur-sm text-black flex-1 p-8 prose-ul:list-disc prose-ul:font-medium prose-ol:list-decimal prose-ol:font-medium prose-base prose-p:font-medium prose-headings:my-1 prose-headings:font-semibold prose-a:text-blue-600 prose-a:underline hover:prose-a:text-blue-800 mx-auto sm:ml-48">
+				<main className="backdrop-blur-sm text-black flex-1 p-8 prose-ul:list-disc prose-ul:font-medium prose-ol:list-decimal prose-ol:font-medium prose-base prose-p:font-medium prose-headings:my-1 prose-headings:font-semibold prose-a:text-blue-600 prose-a:hover:underline prose-a:hover:text-blue-800 mx-auto sm:ml-48">
 					<Content />
 				</main>
 			</div>
