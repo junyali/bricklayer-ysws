47058125d7569d9fa8440d9f7372d214582b3692
Junya <junjia.liu@pm.me>
2025-12-19 08:17:02 +0000

    Woops forgot about you

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,3 +1,4 @@
+import '../styles/App.css';
 import { OrpheusFlag } from '../components/OrpheusFlag.tsx';
 import { Footer } from '../components/Footer.tsx';
 import { NavBar } from '../components/navigation/NavBar.tsx';
