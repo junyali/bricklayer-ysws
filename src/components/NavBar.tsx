ca1b228ada6fcdaeadeb8be8748afb6d7c1f3d1b
Junya <junjia.liu@pm.me>
2025-12-19 04:21:33 +0000

    Use Link instead of a tags (again)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router';
+
 export function NavBar() {
 	return (
 		<nav className="flex gap-3 items-start justify-end">
-			<a
-				href="https://github.com/junyali/bricklayer-ysws"
+			<Link
+				to="https://github.com/junyali/bricklayer-ysws"
 				target="_blank"
 				rel="noopener noreferrer"
 				className="relative block w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 cursor-pointer -translate-y-2 hover:translate-y-0 transition-transform duration-200 ease-in-out"
@@ -13,9 +15,9 @@ export function NavBar() {
 					className="w-full h-full"
 				></img>
 				<i className="fa-brands fa-github fa-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white"></i>
-			</a>
-			<a
-				href="/guide"
+			</Link>
+			<Link
+				to="/guide"
 				className="relative block w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 cursor-pointer -translate-y-2 hover:translate-y-0 transition-transform duration-200 ease-in-out"
 			>
 				<img
@@ -24,7 +26,7 @@ export function NavBar() {
 					className="w-full h-full"
 				></img>
 				<i className="fa-solid fa-book fa-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white"></i>
-			</a>
+			</Link>
 		</nav>
 	);
 }
