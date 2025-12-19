1cfe941951effa9af7e853fb0dd7415648c49a5a
Junya <junjia.liu@pm.me>
2025-12-19 05:25:29 +0000

    Added toolbar options to canvas

--- src/pages/Canvas.tsx
+++ src/pages/Canvas.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import { OrpheusFlag } from '../components/OrpheusFlag';
 import { Footer } from '../components/Footer';
+import { ToolButton } from '../components/ToolButton';
+
+type Tool = 'brush' | 'eraser'
 
 export function Canvas() {
+	const [selectedTool, setSelectedTool] = useState<Tool>('brush');
+
 	return (
 		<div className="min-h-screen bg-[url(/new_studs.png)] bg-[length:256px_256px] bg-repeat bg-center">
 			<div>
@@ -25,7 +31,21 @@ export function Canvas() {
 					</h1>
 				</div>
 			</div>
-			<div className="flex flex-1">
+			<div className="flex flex-1 relative items-center">
+				<aside className="absolute left-0 bg-white border-4 border-l-0 border-black rounded-r-xl shadow-lg p-2 space-y-1 z-10">
+					<ToolButton
+						icon="/canvas/paintbrush.png"
+						label="Brush"
+						isSelected={selectedTool === 'brush'}
+						onClick={() => setSelectedTool('brush')}
+					/>
+					<ToolButton
+						icon="/canvas/eraser.png"
+						label="Eraser"
+						isSelected={selectedTool === 'eraser'}
+						onClick={() => setSelectedTool('eraser')}
+					/>
+				</aside>
 				<main className="flex-1 flex items-center justify-center p-8">
 					<div
 						className="bg-[url(/universal_bright_white.png)] bg-[length:256px_256px] bg-repeat bg-center border-4 border-black rounded-lg shadow-2xl"
