804d2598597c60c7dc271b95999967918334d59b
Junya <junjia.liu@pm.me>
2025-12-19 03:56:55 +0000

    Add gamma correction to bg image

--- src/layouts/DocPage.tsx
+++ src/layouts/DocPage.tsx
@@ -12,7 +12,7 @@ export function DocPage({ Content, SideBar }: Properties) {
 				<aside className="hidden sm:block w-48 fixed h-full">
 					<SideBar />
 				</aside>
-				<main className="bg-[url(/new_studs.png)] bg-[length:512px_512px] text-black flex-1 p-8 prose-ul:list-disc prose-ul:font-medium prose-ol:list-decimal prose-ol:font-medium prose-base prose-p:font-medium prose-headings:my-1 prose-headings:font-semibold prose-a:text-blue-600 prose-a:hover:underline prose-a:hover:text-blue-800 mx-auto sm:ml-48">
+				<main className="bg-[url(/new_studs.png)] bg-[length:512px_512px] brightness-110 text-black flex-1 p-8 prose-ul:list-disc prose-ul:font-medium prose-ol:list-decimal prose-ol:font-medium prose-base prose-p:font-medium prose-headings:my-1 prose-headings:font-semibold prose-a:text-blue-600 prose-a:hover:underline prose-a:hover:text-blue-800 mx-auto sm:ml-48">
 					<Content />
 				</main>
 			</div>
