0dd46866bd29d564aa87673d4a8f83c44d4c66e4
Junya <junjia.liu@pm.me>
2025-12-20 15:24:48 +0000

    Update footer decoration and formatting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,32 @@ export function Footer() {
 			<div
 				className="bg-red-600 py-12 text-center bg-[url(/weld_bright_red.png)] bg-[length:512px_512px] bg-repeat bg-center text-2xl"
 			>
-				<p className="font-bold text-white text-shadow-sm text-shadow-pink-400">made with ❤️ by @junya // follow me on <a href="https://github.com/junyali" target="_blank" className="text-sky-300 text-blue hover:text-blue-100 underline decoration underline-offset-2 transition-colors">github</a>!</p>
+				<p
+					className="font-bold text-white text-shadow-sm text-shadow-pink-400"
+				>
+					<span>made with ❤️ by</span>
+					{' '}
+					<a
+						href="https://hackclub.enterprise.slack.com/team/U082PAFDDS5"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-white hover:text-gray-400 transition-colors"
+					>
+						@junya
+					</a>
+					{' '}
+					<span>// follow me on</span>
+					{' '}
+					<a
+						href="https://github.com/junyali"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-sky-300 text-blue hover:text-blue-100 underline decoration underline-offset-2 transition-colors"
+					>
+						github
+					</a>
+					<span>!</span>
+				</p>
 			</div>
 		</footer>
 	);
