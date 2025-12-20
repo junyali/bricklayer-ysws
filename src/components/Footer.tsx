e4fa45b09a59686487373db67ba3f8cfcf865537
Junya <junjia.liu@pm.me>
2025-12-20 15:13:23 +0000

    Change this colour cause it's ugli

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
 			<div
 				className="bg-red-600 py-12 text-center bg-[url(/weld_bright_red.png)] bg-[length:512px_512px] bg-repeat bg-center text-2xl"
 			>
-				<p className="font-bold text-white text-shadow-sm text-shadow-pink-400">made with ❤️ by @junya // follow me on <a href="https://github.com/junyali" target="_blank" className="text-yellow-300 hover:text-yellow-100 underline decoration underline-offset-2 transition-colors">github</a>!</p>
+				<p className="font-bold text-white text-shadow-sm text-shadow-pink-400">made with ❤️ by @junya // follow me on <a href="https://github.com/junyali" target="_blank" className="text-sky-300 text-blue hover:text-blue-100 underline decoration underline-offset-2 transition-colors">github</a>!</p>
 			</div>
 		</footer>
 	);
