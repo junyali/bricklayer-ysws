b88a0f57a7e4995b2a3d141a8dc6f2c04e59a606
Junya <junjia.liu@pm.me>
2025-12-22 01:35:09 +0000

    Fix text wrapping on BrickButton component

--- src/components/BrickButton.tsx
+++ src/components/BrickButton.tsx
@@ -11,7 +11,7 @@ export function BrickButton({ href, text, className = ''}: Properties) {
 		<div className="flex justify-center my-4">
 			<Link
 				to={href}
-				className={`text-xl sm:text-2xl text-center bg-red-500 text-slate-50 px-3 py-4 rounded-sm font-semibold border-black border-2 bg-repeat bg-center shadow-black shadow-sm transition-all duration-300 hover:scale-105 hover:shadow-xl active:shadow-md hover:border-red-900 hover:-translate-y-1 active:translate-y-0 active:scale-100 cursor-pointer inline-block ${className}`}
+				className={`text-xl sm:text-2xl text-center bg-red-500 text-slate-50 px-3 py-4 rounded-sm font-semibold border-black border-2 bg-repeat bg-center shadow-black shadow-sm transition-all duration-300 hover:scale-105 hover:shadow-xl active:shadow-md hover:border-red-900 hover:-translate-y-1 active:translate-y-0 active:scale-100 cursor-pointer inline-block whitespace-nowrap ${className}`}
 				style={{ backgroundImage: 'url(/box_stud_bright_red.png)' }}
 			>
 				<p
