a999c07321d9ea37355e86b73749688ac50aeae5
Junya <junjia.liu@pm.me>
2025-12-22 03:47:04 +0000

    Playing around with info.ts

--- src/data/info.ts
+++ src/data/info.ts
@@ -8,14 +8,20 @@ export interface Properties {
 export const infoData: Properties[] = [
 	{
 		id: "first-info",
-		image: "/confused_orpheus.png",
+		image: "/dinos/confused_orpheus.png",
 		title: "Roblox.. really?!?!",
 		description: "Yeah we know. <i>That</i> Roblox. Powering those memes and obbies is a surprisingly powerful game engine. Multiplayer, physics, UI, data storage - all out of the box. No infrastructure needed! It's one of the fastest ways to go from \"idea\" to \"playable multiplayer game\", even if you're new to game dev!<br><br><br><br><br><br><br><br><br><br><br>",
 	},
 	{
 		id: "second-info",
-		image: "/confused_dinosaur.png",
+		image: "/dinos/dino_hesitating.png",
 		title: "meow",
 		description: "mrrp <br> nyaaa <br><br><br><br><br><br><br><br><br><br><br><br><br> meow"
-	}
+	},
+	{
+		id: "third-info",
+		image: "/dinos/dino_sees_something_she_wants.png",
+		title: "meow",
+		description: "mrrp <br> nyaaa <br><br><br><br><br><br><br><br><br><br><br><br><br> meow"
+	},
 ]
